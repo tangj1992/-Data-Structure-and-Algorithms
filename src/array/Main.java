88a0f7a735792e18d556ecb2283f867e2aab96a3
tangjun <694756023@qq.com>
2019-06-24 14:20:53 +0800

    有序数组合并

--- src/array/Main.java
+++ src/array/Main.java
@@ -2,7 +2,7 @@ package array;
 
 public class Main{
     public static void main(String[] args){
-        orderedArrayTest();
+        mergeSort();
     }
 
     private static void genericArrayTest(){
@@ -21,10 +21,52 @@ public class Main{
 
     private static void orderedArrayTest(){
         OrderedArray<Integer> arr = new OrderedArray<>(3, OrderedArray.ASC);
-        System.out.println(arr.insert(3));
-        System.out.println(arr.insert(1));
-        System.out.println(arr.insert(2));
+        arr.insert(3);
+        arr.insert(1);
+        arr.insert(2);
         System.out.println(arr);
+
+        OrderedArray<Integer> arr2 = new OrderedArray<>(3, OrderedArray.ASC);
+        arr2.insert(3);
+        arr2.insert(2);
+        arr2.insert(5);
+        System.out.println(arr2);
+
+    }
+
+    private static void mergeSort(){
+        int[] arr1 = new int[]{1,2,3};
+        int[] arr2 = new int[]{3,4,5};
+        int[] newArr = new int[arr1.length + arr2.length];
+
+        int index1 = 0, index2 = 0, index = 0;
+
+        while (index1 < arr1.length && index2 < arr2.length){
+            if (arr1[index1] > arr2[index2]){
+                newArr[index] = arr2[index2];
+                index2++;
+            }else{
+                newArr[index] = arr1[index1];
+                index1++;
+            }
+            index++;
+        }
+        while (index1 < arr1.length){
+            newArr[index] = arr1[index1];
+            index++;
+            index1++;
+        }
+
+        while (index2 < arr2.length){
+            newArr[index] = arr2[index2];
+            index++;
+            index2++;
+        }
+
+        for (int i : newArr) {
+            System.out.println(i);
+        }
+
     }
 
 }
